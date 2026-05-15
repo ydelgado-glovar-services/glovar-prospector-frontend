e9222286ba3adad4f2360d0025dcba759f86c989
YeisonDelgado <yeisondelgado@unicauca.edu.co>
2026-05-15 13:07:08 -0500

    Refactor: UX

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
